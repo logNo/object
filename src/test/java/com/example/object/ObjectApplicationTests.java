9857e135fde7e3f5d3a789caac4107ba2f887fcc
logNo <lusiue@naver.com>
2019-06-20 13:29:46 +0900

    init project

---
+++ src/test/java/com/example/object/ObjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.object;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ObjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
